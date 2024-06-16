80e96b298bccc5c4f94228bd3b995466de3ddc0b
Julik Tarkhanov <me@julik.nl>
2024-06-16 13:01:57 +0200

    Explain the presence of GoudaAdapter

--- lib/active_job/queue_adapters/gouda_adapter.rb
+++ lib/active_job/queue_adapters/gouda_adapter.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-module ActiveJob # :nodoc:
-  module QueueAdapters # :nodoc:
-    class GoudaAdapter < Gouda::Adapter
-    end
-  end
+# The sole purpose of this class is so that you can do
+# `config.active_job.queue_adapter = :gouda` in your Rails
+# config, as Rails insists on resolving the adapter module
+# name from the symbol automatically. If Rails ever allows
+# us to "register" an adapter to a symbol this module can
+# be removed later.
+module ActiveJob::QueueAdapters::GoudaAdapter < Gouda::Adapter
 end
