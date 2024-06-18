722bb8e6aa2f32e7fe4573e189ec02a022a79392
Julik Tarkhanov <me@julik.nl>
2024-06-18 17:44:41 +0200

    Improve documentation a bit (#10)

--- lib/gouda/bulk.rb
+++ lib/gouda/bulk.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 module Gouda
+  # Inside this call, all `perform_later` calls on ActiveJob subclasses
+  # (including mailers) will be buffered. The call is reentrant, so you
+  # can have multiple `in_bulk` calls with arbitrary nesting. At the end
+  # of the block, the buffered jobs will be enqueued using their respective
+  # adapters. If an adapter supports `enqueue_all` (Sidekiq does in recent
+  # releases of Rails, for example), this functionality will be used. This
+  # method is especially useful when doing things such as mass-emails, or
+  # maintenance tasks where a large swath of jobs gets enqueued at once.
+  #
+  # @example
+  #    Gouda.in_bulk do
+  #      User.recently_joined.find_each do |recently_joined_user|
+  #        GreetingJob.perform_later(recently_joined_user)
+  #      end
+  #    end
+  # @return [Object] the return value of the block
   def self.in_bulk(&blk)
     if Thread.current[:gouda_bulk_buffer].nil?
       Thread.current[:gouda_bulk_buffer] = []
